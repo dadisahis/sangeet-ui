f3d507bdb17019e7b0b9d7d74e48fc23c73842da
Adithya Saish <adiwithmj@gmail.com>
2023-02-15 00:07:58 +0530

    UI updates

--- src/components/ConversationList/ConversationList.jsx
+++ src/components/ConversationList/ConversationList.jsx
@@ -4,6 +4,7 @@ import Chatbox from "../Chatbox/Chatbox";
 import ConversationItem from "../ConversationItem/ConversationItem";
 import { io } from "socket.io-client";
 import { useRef } from "react";
+import { Cancel } from "@mui/icons-material";
 /*
 get users using conversation reciever_id
 pass the user object ConverstionItem
@@ -11,6 +12,8 @@ pass the user object ConverstionItem
 
 function ConversationList({ conversations }) {
   const [currentConversation, setCurrentConversation] = useState(null);
+  const [openChat, setOpenChat] = useState(false);
+  let windowSize = window.screen.width;
   const socket = useRef();
   useEffect(() => {
     socket.current = io(process.env.REACT_APP_SOCKET_URI);
@@ -26,7 +29,12 @@ function ConversationList({ conversations }) {
   }, [conversations]);
   return (
     <div className="chat_list">
-      <div className="chat_list_left">
+      <div
+        className="chat_list_left"
+        style={{
+          display: windowSize > 500 ? "flex" : openChat ? "none" : "flex",
+        }}
+      >
         <div className="list_top">
           <p className="title">Messages</p>
         </div>
@@ -34,7 +42,10 @@ function ConversationList({ conversations }) {
           {conversations.map((conversation) => (
             <div
               className="conversation_item_container"
-              onClick={() => setCurrentConversation(conversation)}
+              onClick={() => {
+                setCurrentConversation(conversation);
+                setOpenChat(true);
+              }}
             >
               <ConversationItem
                 conversation={conversation}
@@ -49,9 +60,19 @@ function ConversationList({ conversations }) {
           ))}
         </div>
       </div>
-      <div className="chat_list_right">
+      <div
+        className="chat_list_right"
+        style={{
+          display: windowSize > 500 ? "flex" : openChat ? "flex" : "none",
+        }}
+      >
         {currentConversation ? (
-          <Chatbox conversation={currentConversation} socket={socket} />
+          <>
+            <Chatbox conversation={currentConversation} socket={socket} />
+            <div className="cross_button" onClick={() => setOpenChat(false)}>
+              <Cancel />
+            </div>
+          </>
         ) : null}
       </div>
     </div>
